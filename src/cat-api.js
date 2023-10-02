df78b4eff29a8502392eeadca8f77325a1437e7b
SerhiiPochtovyk <sergey.po4tovik@gmail.com>
2023-10-02 17:47:22 +0300

    up files;

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,18 +1,30 @@
 import axios from 'axios';
-const BASE_URL = 'https://api.thecatapi.com/v1';
-
 axios.defaults.headers.common['x-api-key'] =
   'live_SWwNDJ6lixvIgMTFSA1O4wT3iaw1WygGWikOVhdwUP1RCHZe5g4W3gX9Bf2NR5Jv';
 
-// https://api.thecatapi.com/v1/images/search?breed_ids={breed.id}
-// basic filter
-
 export function fetchBreeds() {
-  return axios.get(`${BASE_URL}/breeds`).then(response => response.data);
+  const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
+  const url = `${BASE_URL}`;
+  return axios.get(url).then(response => {
+    if (!response.ok) {
+      return response.data;
+    } else {
+      throw new Error(
+        `Oops! Something went wrong! Try reloading the page! (${response.status})`
+      );
+    }
+  });
 }
 
-export function fetchCatByBreed(breedId) {
-  return axios
-    .get(`${BASE_URL}/images/search?breed_ids=${breedId}`)
-    .then(response => response.data);
-}
+export const fetchCatByBreed = selectedAnimal => {
+  const catBaseUrl = `https://api.thecatapi.com/v1/images/search?breed_ids=${selectedAnimal}`;
+  return axios.get(catBaseUrl).then(response => {
+    if (!response.ok) {
+      return response.data;
+    } else {
+      throw new Error(
+        `Oops! Something went wrong! Try reloading the page! (${response.status})`
+      );
+    }
+  });
+};
--- src/index.js
+++ src/index.js
@@ -1,103 +1,77 @@
-import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
-import Swal from 'sweetalert2';
-import SlimSelect from 'slim-select';
-import 'slim-select/dist/slimselect.css';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
-const breedSelect = document.querySelector('.breed-select');
-const catInfo = document.querySelector('.cat-info');
-const loader = document.querySelector('.loader');
-const error = document.querySelector('.error');
+const refs = {
+  selectEl: document.querySelector('.breed-select'),
+  catInfoEl: document.querySelector('.cat-info'),
+  loaderEl: document.querySelector('.loader'),
+  errorEl: document.querySelector('.error'),
+};
 
-catInfo.classList.add('is-hidden');
-
-breedSelect.addEventListener('change', createMarkup);
-
-updateSelect();
-
-function updateSelect(data) {
-  loader.classList.remove('is-hidden');
-
-  fetchBreeds(data)
-    .then(data => {
-      loader.classList.add('is-hidden');
-      breedSelect.classList.remove('is-hidden');
-      breedSelect.innerHTML = '';
-      let markSelect = data.map(({ name, id }) => {
-        return `<option value ='${id}'>${name}</option>`;
-      });
-      breedSelect.insertAdjacentHTML('beforeend', markSelect);
-      new SlimSelect({
-        select: breedSelect,
-      });
-    })
-    .catch(onError);
+function showLoader() {
+  refs.loaderEl.style.display = 'block';
+  refs.selectEl.style.display = 'none';
+  refs.catInfoEl.style.display = 'none';
 }
 
-function createMarkup(event) {
-  loader.classList.remove('is-hidden');
-  catInfo.classList.add('is-hidden');
-
-  const breedId = event.currentTarget.value;
-
-  fetchCatByBreed(breedId)
-    .then(data => {
-      loader.classList.add('is-hidden');
-      breedSelect.classList.remove('is-hidden');
-      const { url, breeds } = data[0];
-
-      catInfo.innerHTML = `<img src="${url}" alt="${breeds[0].name}" width="400"/><div class="box"><h2>${breeds[0].name}</h2><p>${breeds[0].description}</p><p><strong>Temperament:</strong> ${breeds[0].temperament}</p></div>`;
-      catInfo.classList.remove('is-hidden');
-    })
-    .catch(onError);
+function hiddenLoader() {
+  refs.loaderEl.style.display = 'none';
+  refs.selectEl.style.display = 'block';
+  refs.catInfoEl.style.display = 'block';
 }
 
-function onError() {
-  loader.classList.add('is-hidden');
-  breedSelect.innerHTML = '';
+function showError(message) {
+  refs.errorEl.textContent = message;
+}
 
-  Swal.fire({
-    icon: 'error',
-    title: 'Oops!',
-    text: 'Something went wrong! Try reloading the page!',
+function clearError() {
+  refs.errorEl.textContent = '';
+}
+
+clearError();
+showLoader();
+fetchBreeds()
+  .then(data => {
+    if (data) {
+      data.forEach(breed => {
+        const option = document.createElement('option');
+        option.value = breed.id;
+        option.textContent = breed.name;
+        refs.selectEl.appendChild(option);
+      });
+    } else {
+      throw new Error('Oops! Something went wrong! Try reloading the page!');
+    }
+    hiddenLoader();
+  })
+  .catch(err => {
+    showError(err.message);
+    hiddenLoader();
   });
-}
 
-// ======================================== ++ comments ++ =====================================================
+export const onSelectElChange = event => {
+  const selectedAnimal = event.target.value;
+  clearError();
+  showLoader();
+  fetchCatByBreed(selectedAnimal)
+    .then(data => {
+      if (data) {
+        const catImageUrl = data[0].url;
+        const catName = data[0].breeds[0].name;
+        const catDescr = data[0].breeds[0].description;
 
-// import { fetchBreeds, fetchCatByBreed } from './js/cat-api'; означає, що ми імпортуємо функції fetchBreeds та fetchCatByBreed з модуля cat-api, який знаходиться у папці js.
+        const markUp = `<img src="${catImageUrl}" height=400 width="600"/>
+         <h2 class="cats-name">${catName}</h2> <p>${catDescr}</p>`;
 
-// Наступний рядок import Swal from 'sweetalert2'; означає, що ми імпортуємо бібліотеку "sweetalert2" у змінну Swal.
+        refs.catInfoEl.innerHTML = markUp;
+      } else {
+        throw new Error('Oops! Something went wrong! Try reloading the page!');
+      }
+      hiddenLoader();
+    })
+    .catch(err => {
+      showError(err.message);
+      hiddenLoader();
+    });
+};
 
-// Рядок import SlimSelect from 'slim-select'; імпортує бібліотеку "slim-select" у змінну SlimSelect.
-
-// Рядок 'slim-select/dist/slimselect.css'; імпортує CSS файл для стилізації slim-select.
-
-// Наступні рядки оголошують змінні breedSelect, catInfo, loader та error, та вибирають їх з DOM верстки за допомогою document.querySelector().
-
-// Рядок catInfo.classList.add('is-hidden'); додає клас .is-hidden до елементу з класом cat-info, що робить його невидимим.
-
-// Наступний рядок breedSelect.addEventListener('change', createMarkup); додає подію слухача на елементі breedSelect, який реагує на зміну значення.
-
-// Рядок updateSelect(); викликає функцію updateSelect без передачі якого-небудь аргументу.
-
-// Функція updateSelect приймає аргумент data. Запускається loader.classList.remove('is-hidden');, що видаляє клас '.is-hidden' з елемента loader щоб зробити його видимим.
-
-// За допомогою функції fetchBreeds ми отримуємо дані про породи котів. Коли дані приходять, виконується ланцюжок .then де ми робимо наступні дії.
-
-// Також видаляється клас '.is-hidden' з елемента loader, та видаляється клас '.is-hidden' з елемента breedSelect.
-
-// Видаляється все вміст з елемента breedSelect, та формується рядок з опціями в markSelect за допомогою методу map, що об'єднує name та id у потрібному форматі. Потім рядок markSelect додається у елемент breedSelect за допомогою breedSelect.insertAdjacentHTML('beforeend', markSelect).
-
-// Створюється новий екземпляр SlimSelect, якому передається елемент breedSelect.
-
-// Функція createMarkup приймає event об'єкт. Запускається loader.classList.remove('is-hidden'); щоб показати заритування.
-
-// Отримуємо breedId, який представляє значення, вибране в breedSelect.
-
-// За допомогою функції fetchCatByBreed ми отримуємо дані про кота обраної породи. Коли дані приходять, виконується ланцюжок .then де ми робимо наступні дії.
-
-// Також видаляється клас '.is-hidden' з елемента loader, та видаляється клас '.is-hidden' з елемента breedSelect.
-
-// З отриманих даних ми витягуємо url та breeds. Далі форматуємо HTML рядок, який включає зображення, ім'я, опис і поведінку кота. Вся ця інформація додається у елемент catInfo, та видаляється клас '.is-hidden', щоб зробити його видимим.
-
-// Функція onError спрацьовує, якщо виникає помилка. Вона додає клас '.is-hidden' до елемента loader, та видаляє весь вміст у елемента breedSelect. Виводиться модальне вікно з помилкою.
+refs.selectEl.addEventListener('change', onSelectElChange);
